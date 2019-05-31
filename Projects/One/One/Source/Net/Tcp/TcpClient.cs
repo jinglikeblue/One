12d7a35004b22e83bee2cd1df032bddd7108eb56
Jing <122300890@qq.com>
2019-05-31 23:10:38 +0800

    开始尝试将项目改为单线程模型的。使用起来简单、安全。

--- Projects/One/One/Source/Net/Tcp/TcpClient.cs
+++ Projects/One/One/Source/Net/Tcp/TcpClient.cs
@@ -5,7 +5,7 @@ using System.Net.Sockets;
 
 namespace One
 {
-    public class TcpClient : IRemoteProxy
+    public class TcpClient<ProtocolProcess> : IRemoteProxy
     {
         /// <summary>
         /// 连接成功事件(多线程事件）
@@ -20,12 +20,31 @@ namespace One
         /// <summary>
         /// 连接失败事件(多线程事件）
         /// </summary>
-        public event EventHandler<IRemoteProxy> onConnectFail;
+        public event EventHandler<IRemoteProxy> onConnectFail;        
 
+        /// <summary>
+        /// 接收数据的SocketAsyncEventArgs
+        /// </summary>
         SocketAsyncEventArgs _receiveEA;
+
+        /// <summary>
+        /// 发送数据的SocketAsyncEventArgs
+        /// </summary>
         SocketAsyncEventArgs _sendEA;
+
+        /// <summary>
+        /// 线程同步器，将异步方法同步到调用Refresh的线程中
+        /// </summary>
+        ThreadSyncActions _tsa = new ThreadSyncActions();
+
+        /// <summary>
+        /// Socket对象
+        /// </summary>
         protected Socket _socket;
 
+        /// <summary>
+        /// 接收数据的缓冲区
+        /// </summary>
         protected byte[] _receiveBuffer;
 
         /// <summary>
@@ -48,7 +67,14 @@ namespace One
         /// </summary>
         public IProtocolProcess protocolProcess { get; internal set; }
 
+        /// <summary>
+        /// 主机地址
+        /// </summary>
         public string Host { get; private set; }
+
+        /// <summary>
+        /// 主机端口
+        /// </summary>
         public int Port { get; private set; }
 
         /// <summary>
@@ -93,6 +119,38 @@ namespace One
             Reconnect();
         }
 
+        public void Reconnect()
+        {
+            Close();
+
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(Host), Port);
+            Socket socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            SocketAsyncEventArgs connectEA = new SocketAsyncEventArgs();
+            connectEA.RemoteEndPoint = ipe;
+            connectEA.Completed += OnConnectCompleted;
+            if (!socket.ConnectAsync(connectEA))
+            {
+                OnConnectCompleted(null, connectEA);
+            }
+        }
+
+        /// <summary>
+        /// 刷新
+        /// </summary>
+        public void Refresh()
+        {
+            _tsa.RunSyncActions();
+        }
+
+        /// <summary>
+        /// 断开客户端连接，并且不触发任何事件
+        /// </summary>
+        public void CloseSilently()
+        {
+            onDisconnect = null;
+            Close();
+        }
+
         /// <summary>
         /// 断开客户端连接
         /// </summary>
@@ -114,35 +172,6 @@ namespace One
             }
         }
 
-        public void Reconnect()
-        {
-            Close();
-
-            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(Host), Port);
-            Socket socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            SocketAsyncEventArgs connectEA = new SocketAsyncEventArgs();
-            connectEA.RemoteEndPoint = ipe;
-            connectEA.Completed += OnConnectCompleted;
-            if (!socket.ConnectAsync(connectEA))
-            {
-                OnConnectCompleted(null, connectEA);
-            }
-        }
-
-        protected virtual void OnConnectCompleted(object sender, SocketAsyncEventArgs e)
-        {
-            e.Completed -= OnConnectCompleted;
-            if (null == e.ConnectSocket)
-            {
-                DispatchConnectFailEvent();
-                return;
-            }
-
-            _socket = e.ConnectSocket;
-            DispatchConnectSuccessEvent();
-            StartReceive();
-        }
-
         protected void DispatchConnectFailEvent()
         {
             onConnectFail?.Invoke(this, this);
@@ -151,64 +180,6 @@ namespace One
         protected void DispatchConnectSuccessEvent()
         {
             onConnectSuccess?.Invoke(this, this);
-        }
-
-        /// <summary>
-        /// 开始接受数据
-        /// </summary>
-        protected void StartReceive()
-        {
-            if(IsFade)
-            {
-                return;
-            }
-
-            _receiveEA.SetBuffer(_receiveBuffer, _bufferAvailable, _receiveBuffer.Length - _bufferAvailable);
-
-            if (!_socket.ReceiveAsync(_receiveEA))
-            {
-                OnReceiveCompleted(null, _receiveEA);
-            }
-        }
-
-        /// <summary>
-        /// 处理接收到的消息（多线程事件）
-        /// </summary>        
-        void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
-        {
-            if (e.BytesTransferred > 0 && e.SocketError == SocketError.Success)
-            {
-                _bufferAvailable += e.BytesTransferred;
-
-                ProcessReceivedData();
-
-                StartReceive();
-            }
-            else
-            {
-                Close();
-            }
-        }
-
-        /// <summary>
-        /// 处理接收到的数据
-        /// </summary>
-        protected virtual void ProcessReceivedData()
-        {
-            //协议处理器处理协议数据
-            int used = protocolProcess.Unpack(_receiveBuffer, _bufferAvailable);
-
-            if (used > 0)
-            {
-                _bufferAvailable = _bufferAvailable - used;
-                if (0 != _bufferAvailable)
-                {
-                    //将还没有使用的数据移动到数据开头
-                    byte[] newBytes = new byte[_receiveBuffer.Length];
-                    Array.Copy(_receiveBuffer, used, newBytes, 0, _bufferAvailable);
-                    _receiveBuffer = newBytes;
-                }
-            }
         }
 
         /// <summary>
@@ -223,7 +194,6 @@ namespace One
                 {
                     return;
                 }
-
 
                 _sendBufferList.Add(new ArraySegment<byte>(bytes));
 
@@ -250,9 +220,97 @@ namespace One
             }
         }
 
+        /// <summary>
+        /// 处理接收到的数据
+        /// </summary>
+        protected virtual void ProcessReceivedData()
+        {
+            //协议处理器处理协议数据
+            int used = protocolProcess.Unpack(_receiveBuffer, _bufferAvailable);
+
+            if (used > 0)
+            {
+                _bufferAvailable = _bufferAvailable - used;
+                if (0 != _bufferAvailable)
+                {
+                    //将还没有使用的数据移动到数据开头
+                    byte[] newBytes = new byte[_receiveBuffer.Length];
+                    Array.Copy(_receiveBuffer, used, newBytes, 0, _bufferAvailable);
+                    _receiveBuffer = newBytes;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 开始接受数据
+        /// </summary>
+        protected void StartReceive()
+        {
+            if (IsFade)
+            {
+                return;
+            }
+
+            _receiveEA.SetBuffer(_receiveBuffer, _bufferAvailable, _receiveBuffer.Length - _bufferAvailable);
+
+            if (!_socket.ReceiveAsync(_receiveEA))
+            {
+                OnReceiveCompleted(null, _receiveEA);
+            }
+        }
+
+        /// <summary>
+        /// 连接完成（多线程事件）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void OnConnectCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            _tsa.AddToSyncAction(() =>
+            {
+                e.Completed -= OnConnectCompleted;
+                if (null == e.ConnectSocket)
+                {
+                    DispatchConnectFailEvent();
+                    return;
+                }
+
+                _socket = e.ConnectSocket;
+                DispatchConnectSuccessEvent();
+                StartReceive();
+            });
+        }
+
+        /// <summary>
+        /// 处理接收到的消息（多线程事件）
+        /// </summary>        
+        void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            _tsa.AddToSyncAction(() =>
+            {
+                if (e.BytesTransferred > 0 && e.SocketError == SocketError.Success)
+                {
+                    _bufferAvailable += e.BytesTransferred;
+
+                    ProcessReceivedData();
+
+                    StartReceive();
+                }
+                else
+                {
+                    Close();
+                }
+            });
+        }
+
+        /// <summary>
+        /// 消息发送完毕（多线程事件）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void OnSendCompleted(object sender, SocketAsyncEventArgs e)
         {
-            lock (this)
+            _tsa.AddToSyncAction(() =>
             {
                 if (e.SocketError == SocketError.Success)
                 {
@@ -264,7 +322,7 @@ namespace One
                 {
                     Close();
                 }
-            }
+            });
         }
 
         /// <summary>
