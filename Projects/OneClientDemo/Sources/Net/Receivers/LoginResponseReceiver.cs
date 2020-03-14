364645e2936ddd774ffb64de716419e24a686263
Jing <122300890@qq.com>
2020-03-14 23:53:23 +0800

    update

--- Projects/OneClientDemo/Sources/Net/Receivers/LoginResponseReceiver.cs
+++ Projects/OneClientDemo/Sources/Net/Receivers/LoginResponseReceiver.cs
@@ -7,7 +7,7 @@ namespace OneClient
     class LoginResponseReceiver : BaseClientReceiver<LoginResponseVO>
     {
         protected override void OnReceive(Client client, LoginResponseVO obj)
-        {
+        {            
             OneLog.D("登录返回：{0}", obj.id);
         }
     }
